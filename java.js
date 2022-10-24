266ceba68b779310d949757b16834e94360434f5
YoussefGotar <115504042+YoussefGotar@users.noreply.github.com>
2022-10-24 21:36:07 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,8 @@
+let img0=document.querySelector('.img0');
+let conteneir=document.querySelector('.conteneir');
+function change(phone){
+    img0.src=phone;
+}
+function change_color(color){
+   conteneir.style.background=color;
+}
